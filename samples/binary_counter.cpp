67cc35a16afeb5a5970d9ff4e04200378b7eed15
Shane Gough <shane@sensaura.org>
2015-09-04 14:57:36 +1000

    Started adding sample code.

---
+++ samples/binary_counter.cpp
@@ -0,0 +1,50 @@
+/*--------------------------------------------------------------------------*
+* Sample SensNode main program
+*---------------------------------------------------------------------------*
+* 03-Sep-2015 ShaneG
+*
+* This sample simply uses the 8 digital output bins as a binary counter
+* output incrementing the count every 250 milliseconds.
+*--------------------------------------------------------------------------*/
+#include <sensnode.h>
+
+// Last activity timestamp
+static uint32_t g_timer;
+
+// Current pin state
+static uint8_t g_state = 0;
+
+/** User application initialisation
+ *
+ * The library will call this function once at startup to allow the user
+ * application to do any initialisation it needs. At the time this function
+ * is called all IO pins will have been set to their default states and the
+ * network subsystem initialised (if not yet connected).
+ */
+void setup() {
+  // Set all pins as output
+  for(int pin=PIN_D0; pin<=PIN_D7; pin++) {
+    digitalInit(pin, OUTPUT);
+    digitalWrite(pin, false);
+    }
+  g_timer = getTicks();
+  }
+
+/** User application loop
+ *
+ * The library repeatedly calls this function in an endless loop. The function
+ * will generally be implemented as a state machine and take care to minimise
+ * the amount of time spent in the function itself.
+ */
+void loop() {
+  // Update state if needed
+  if(timeExpired(g_timer, 250, MILLISECOND)) {
+    g_timer = getTicks();
+    g_state++;
+    // Update pin output
+    DBG("Updating pin output");
+    for(int pin=PIN_D0; pin<=PIN_D7; pin++)
+      digitalWrite(pin, g_state & (1 << pin));
+    }
+  }
+
