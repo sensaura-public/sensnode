896ae57f32fc3957300d44dac18604f966b5efb1
Shane Gough <shane@sensaura.org>
2015-09-04 11:42:02 +1000

    Adding interface for serial port and some simple debugging macros.

---
+++ firmware/lpc1114/serial.cpp
@@ -0,0 +1,14 @@
+/*---------------------------------------------------------------------------*
+* LPC1114 implementation of serial port and debugging
+*----------------------------------------------------------------------------*
+* 04-Sep-2015 ShaneG
+*
+* Blank implementation for now
+*---------------------------------------------------------------------------*/
+#include <sensnode.h>
+#include <platform.h>
+
+// No implementation yet
+OutputStream *debug = NULL;
+Serial *serial = NULL;
+
