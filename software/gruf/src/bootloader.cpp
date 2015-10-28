ceface4f2f24442f5e63306bb849eb3b86dc5b18
Shane Gough <shane@sensaura.org>
2015-10-28 11:28:26 +1000

    Refactoring bootloader interface.

--- software/gruf/src/bootloader.cpp
+++ software/gruf/src/bootloader.cpp
@@ -11,48 +11,60 @@
 #include <stdbool.h>
 #include <gruf.h>
 
-//--- TODO: Bootloader implementation functions
-
 // Maximum length of a device name
 #define MAX_DEVICE_NAME 64
 
-/** Map device name to device information
+/** Bootloader factory function signature
+ *
+ * Bootloader implementations must provide functions that match this signature
+ * to create instances of the Bootloader interface.
+ *
+ * @param cszDevice the name of the target device
+ * @param id1 the first part of the expected device ID
+ * @param id2 the second part of the expected device ID
+ *
+ * @return a Bootloader instance or NULL on error.
  */
-struct DeviceInfoDetail {
-  const char *m_cszName; //! Name of the device
-  DeviceInfo  m_info;    //! Information about the device
+typedef Bootloader *(*PFN_BOOTLOADER_FACTORY)(const char *cszDevice, uint32_t id1, uint32_t id2);
+
+/** Information required to flash a device
+ */
+struct DeviceInfo {
+  const char            *m_cszName;    //! Name of the device
+  PFN_BOOTLOADER_FACTORY m_pfnFactory; //!< The bootloader function to call
+  uint32_t               m_id1;        //!< First part of device ID code
+  uint32_t               m_id2;        //!< Second part of device ID code
   };
+
+//--- TODO: Bootloader implementation functions
 
 /** The master device table
  */
-static DeviceInfoDetail g_devices[] = {
-  { NULL, { NULL, 0, 0, 0, 0 } },
+static DeviceInfo g_devices[] = {
+  // Name  FactoryFN  ID1 ID2
+  { NULL,  NULL,      0,  0    },
   };
 
-/** Get the device information structure for the named device
+/** Get the bootloader implementation for the named device
  *
- * Populates a DeviceInfo structure with details about the named device. The
- * device name is specified on the command line and is matched to device
- * identification information and a bootloader protocol implementation.
+ * This function creates an returns an appropriate Bootloader implementation
+ * for the named device.
  *
- * @param cszDevice the name of the device to get info about
- * @param pDeviceInfo pointer to a DeviceInfo structure to fill
+ * @param cszDevice the device name.
  *
- * @return true if the device is known and the information provided, false if
- *              the device is not supported.
+ * @return a bootloader instance that can be used to program device or NULL
+ *         if the device is not recognised.
  */
-bool getDeviceInfo(const char *cszDevice, DeviceInfo *pDeviceInfo) {
+Bootloader *getBootloader(const char *cszDevice) {
   // Check parameters
-  if((cszDevice==NULL)||(pDeviceInfo==NULL))
+  if(cszDevice==NULL)
     return false;
   // Find the device
   for(int i=0; g_devices[i].m_cszName!=NULL; i++) {
-    if(strnicmp(cszDevice, g_devices[i].m_cszName, MAX_DEVICE_NAME)==0) {
-      memcpy(pDeviceInfo, &g_devices[i].m_info, sizeof(DeviceInfo));
-      return true;
-      }
+    if(strnicmp(cszDevice, g_devices[i].m_cszName, MAX_DEVICE_NAME)==0)
+      return (g_devices[i].m_pfnFactory)(g_devices[i].m_cszName, g_devices[i].m_id1, g_devices[i].m_id2);
     }
   // No such device
-  return false;
+  return NULL;
   }
 
