a09439e2eddde414795576fcffc09235fa40a8d2
Shane Gough <shane@sensaura.org>
2015-10-28 14:54:41 +1000

    More work on the top level process. Still a LOT of implementation details to work out.

--- software/gruf/src/intelhex.cpp
+++ software/gruf/src/intelhex.cpp
@@ -10,6 +10,92 @@
 #include <stdbool.h>
 #include <gruf.h>
 
+class FirmwareImpl : public Firmware {
+  public:
+    /** Default constructor
+     */
+    FirmwareImpl() {
+      }
+
+    //-----------------------------------------------------------------------
+    // Public API
+    //-----------------------------------------------------------------------
+
+    /** Get the first block in the firmware
+     *
+     * The Firmware structure maintains a list of blocks that need to be
+     * written to the target systems flash memory. The list is held in ascending
+     * order of address.
+     *
+     * @return a pointer to the first block of memory to be written or NULL if
+     *         there is no data.
+     */
+    virtual Block *first() {
+      return NULL;
+      }
+
+    /** Get the start address of the flash data to be written
+     *
+     * This is the equivalent of first()->m_base.
+     *
+     * @return the start address of the firmware data or INVALID_ADDRESS if no
+     *         data is available.
+     */
+    virtual uint32_t baseAddress() {
+      return INVALID_ADDRESS;
+      }
+
+    /** Get the last address referenced in the firmware
+     *
+     * @return the highest address containing data to be written or INVALID_ADDRESS
+     *         if no data is available.
+     */
+    virtual uint32_t lastAddress() {
+      return INVALID_ADDRESS;
+      }
+
+    /** Get the total size of the firmware
+     *
+     * This returns the total number of bytes in address space covered by the
+     * firmware. This does not necessarily match the number of bytes that needs
+     * to be written to the target.
+     *
+     * @return the total number of bytes covered by the firmware.
+     */
+    virtual uint32_t totalSize() {
+      return 0;
+      }
+
+    /** Get the number of bytes that need to be written to the target flash.
+     *
+     * This method provides the total number of bytes that need to be written
+     * which excludes any memory areas that have no data specified.
+     *
+     * @return the number of bytes to write to the target.
+     */
+    virtual uint32_t flashSize() {
+      return 0;
+      }
+
+    /** Patch the firmware with a new ID code
+     *
+     * SensNode devices have two IDs associated with them - a TYPEID which is
+     * used to determine the type of the sensor and may be shared across
+     * multiple devices and a NODEID which must be unique for each device.
+     * Rather than compile a new firmware image for every node this method
+     * allows an embedded ID to be modified in place prior to flashing.
+     *
+     * @param id the type of ID to change
+     * @param uuid the 16 byte UUID value to insert into the code.
+     *
+     * @return the address at which the ID was found or INVALID_ADDRESS if the
+     *         location could not be determined.
+     */
+    virtual uint32_t patchID(ID id, const uint8_t *uuid) {
+      return INVALID_ADDRESS;
+      }
+  };
+
 /** Load firmware from a Intel Hex file.
  *
  * This function creates a new Firmware instance and populates it from the
@@ -21,6 +107,10 @@
  *         error occurs.
  */
 Firmware *loadFirmware(const char *cszFilename) {
-  return NULL;
+  // TODO: Make sure the file exists and we can read it
+  FirmwareImpl *firmware = new FirmwareImpl();
+  // TODO: Load file contents into Firmware instance
+  // All done
+  return firmware;
   }
 
