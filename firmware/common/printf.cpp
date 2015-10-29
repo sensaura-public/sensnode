2a82d540821ea85101bd37eee42bbd28d1cb74ae
Shane Gough <shane@sensaura.org>
2015-10-29 14:11:40 +1000

    More migration work. Focusing on fleshing out source files for the XMC1100 target.

--- firmware/common/printf.cpp
+++ firmware/common/printf.cpp
@@ -6,7 +6,6 @@
 * Implement the portable string formatting functions.
 *---------------------------------------------------------------------------*/
 #include <sensnode.h>
-#include <stdarg.h>
 
 //---------------------------------------------------------------------------
 // Helper functions
@@ -136,7 +135,7 @@ struct SPRINTF_DATA {
   int   m_index;    //! Index of next character to write
   };
 
-/** Write a single character to the stream
+/** Write a single character to a buffer
  *
  * @param ch the character to write.
  *
@@ -146,6 +145,17 @@ static bool sprintf_putc(char ch, SPRINTF_DATA *pData) {
   if(pData->m_index==pData->m_length)
     return false;
   pData->m_szOutput[pData->m_index++] = ch;
+  return true;
+  }
+
+/** Write a single character to the serial port
+ *
+ * @param ch the character to write.
+ *
+ * @return true if the write was successful
+ */
+static bool serial_putc(char ch, void *pData) {
+  serialWrite(ch);
   return true;
   }
 
@@ -240,4 +250,25 @@ int sprintf(char *szBuffer, int length, const char *cszString, ...) {
     data.m_szOutput[data.m_index] = '\0';
   return result;
   }
+
+/** Print a formatted string to the serial port.
+ *
+ * This function utilises the @see vprintf function to transmit a formatted
+ * string to the serial port.
+ *
+ * The function is blocking and will not return until all characters have been
+ * transmitted.
+ *
+ * @param cszString the format string to use to generate the output.
+ *
+ * @return the number of bytes sent.
+ */
+int serialPrintF(const char *cszString, ...) {
+  va_list args;
+  va_start(args, cszString);
+  int result = vprintf((FN_PUTC)&serial_putc, NULL, cszString, args);
+  va_end(args);
+  return result;
+  }
+
 
