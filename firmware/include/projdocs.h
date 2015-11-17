40b9f200c86a906cb47b8ac846387a623fc384da
Shane Gough <shane@sensaura.org>
2015-11-17 14:50:19 +1000

    Started adding main page documentation.

---
+++ firmware/include/projdocs.h
@@ -0,0 +1,23 @@
+/*---------------------------------------------------------------------------*
+* Project documentation
+*
+* NOTE: Ignore this file, it is used to generate the front page of the HTML
+*       documentation only.
+*---------------------------------------------------------------------------*/
+/** @mainpage
+ *
+ * This is the main page for the HTML documentation.
+ *
+ * @section hal Hardware Abstraction
+ *
+ * The file @see platform.h defines the platform specific initialisation
+ * functions and brings in definitions specific to the target processor.
+ *
+ * @subsection gpio GPIO
+ *
+ * @see initGPIO
+ *
+ * @section ticks System Timer
+ *
+ * @see initTICK
+ */
